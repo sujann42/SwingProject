66e5fa522244c94ae6de1d2801c2fe1de315748b
Sujan L <sujann42@gmail.com>
2020-02-16 16:30:37 -0600

    toolbar class updated with comments

--- src/components/Toolbar.java
+++ src/components/Toolbar.java
@@ -3,16 +3,21 @@ package components;
 import javax.swing.*;
 import java.awt.*;
 
+//Toolbar class extends JPanel class
 public class Toolbar extends JPanel {
 
     private JButton helloButton;
     private JButton byeButton;
 
     public Toolbar() {
+        //creating buttons
         helloButton = new JButton("Hello");
         byeButton = new JButton("Good Bye");
 
+        //creating a layout => [FLOWLAYOUT container]
         setLayout(new FlowLayout(FlowLayout.CENTER));
+        
+        //adding all the buttons to the container.
         add(helloButton);
         add(byeButton);
 
